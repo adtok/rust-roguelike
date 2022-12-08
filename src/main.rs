c162bfaf325b82e2117507eb7d5b148e29b72153
Adam Tokarsky <36281850+adtok@users.noreply.github.com>
2022-12-08 15:36:26 -0500

    hello world rltk

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use rltk::{GameState, Rltk};
+
+struct State {}
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut Rltk) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello, world!");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    use rltk::RltkBuilder;
+    let context = RltkBuilder::simple80x50()
+        .with_title("Roguelike tutorial")
+        .build()
+        .expect("Context errored out");
+
+    let gs = State {};
+    rltk::main_loop(context, gs).expect("Main loop errored out")
 }
